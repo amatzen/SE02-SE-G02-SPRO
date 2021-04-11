4b8a5b3ca7162878cbd8ad394bddc49205d5f378
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2021-04-11 20:47:31 +0200

    Added popup menu for company list item actions.

---
+++ src/main/java/dk/sdu/swe/partials/PopupListMenu.java
@@ -0,0 +1,36 @@
+package dk.sdu.swe.partials;
+
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXPopup;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PopupListMenu extends JFXPopup {
+
+    private JFXListView<Label> listView;
+    private Map<Label, Runnable> options;
+
+    public PopupListMenu(Map<String, Runnable> options) {
+        listView = new JFXListView<>();
+        this.options = new HashMap<>();
+
+        options.forEach((text, runnable) -> {
+            Label label = new Label(text);
+            this.options.put(label, runnable);
+            listView.getItems().add(label);
+        });
+
+        setPopupContent(listView);
+
+        listView.setOnMouseClicked(e -> {
+            Label label = listView.getSelectionModel().getSelectedItem();
+            if (label != null) {
+                this.options.get(label).run();
+            }
+        });
+    }
+
+}
