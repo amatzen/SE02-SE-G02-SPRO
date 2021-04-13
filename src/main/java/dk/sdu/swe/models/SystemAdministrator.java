d9f20f23388b9f13f8588a008799ef55816ef928
Alexander Matzen <alexander@amatzen.dk>
2021-04-13 16:18:15 +0200

    Added ID to user and JSON modifier

--- src/main/java/dk/sdu/swe/models/SystemAdministrator.java
+++ src/main/java/dk/sdu/swe/models/SystemAdministrator.java
@@ -4,30 +4,31 @@ import java.util.Arrays;
 
 public class SystemAdministrator extends User implements IUser {
     private final String[] permissions = {
-            "programmes",
-            "programmes.epg",
-            "programmes.list",
-            "programmes.filter",
-            "companies",
-            "companies.list",
-            "companies.add",
-            "people",
-            "admin",
-            "admin.reviews",
-            "admin.export",
-            "admin.credit_groups"
+        "programmes",
+        "programmes.epg",
+        "programmes.list",
+        "programmes.filter",
+        "companies",
+        "companies.list",
+        "companies.add",
+        "people",
+        "admin",
+        "admin.reviews",
+        "admin.export",
+        "admin.credit_groups"
     };
 
     /**
      * Instantiates a new User.
      *
+     * @param id       the user id
      * @param username the username
      * @param email    the email
      * @param name     the name
      * @throws Exception the exception
      */
-    public SystemAdministrator(String username, String email, String name) throws Exception {
-        super(username, email, name);
+    public SystemAdministrator(int id, String username, String email, String name) throws Exception {
+        super(id, username, email, name);
     }
 
     @Override
