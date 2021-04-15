2bff9697ca3175de590ac965523e954b255bfdae
Alexander Matzen <alexander@amatzen.dk>
2021-04-15 11:44:57 +0200

    Added createUser

--- src/main/java/dk/sdu/swe/data/JSONHandler.java
+++ src/main/java/dk/sdu/swe/data/JSONHandler.java
@@ -1,14 +1,20 @@
 package dk.sdu.swe.data;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.sendgrid.Content;
+import com.sendgrid.Email;
+import dk.sdu.swe.exceptions.UserCreationException;
 import dk.sdu.swe.models.CompanyAdministrator;
 import dk.sdu.swe.models.SystemAdministrator;
 import dk.sdu.swe.models.User;
+import dk.sdu.swe.provider.EmailProvider;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class JSONHandler implements PersistenceContract {
 
@@ -50,7 +56,22 @@ public class JSONHandler implements PersistenceContract {
     }
 
     @Override
-    public void createUser(User user) {
+    public void createUser(User user) throws Exception {
+        List<User> users = getUsers();
+
+        User match = users.stream().filter(x -> Objects.equals(x.getUsername(), user.getUsername())).findAny().orElse(null);
+        if(match != null) {
+            throw new UserCreationException("Username taken.");
+        }
+
+        JSONObject json = User.userToJson(user);
+
+        String password = User.createRandomPassword(12);
+        String passwordHash = BCrypt.withDefaults().hashToString(12, password.toCharArray());
+
+        json.put("password", passwordHash);
+        Content content = new Content("text/plain", "Velkommen til CrMS - et semesterprojekt!\n\nDu kan nu logge ind i CrMS med foelgende brugeroplysninger:\n\nBrugernavn: " + json.getString("username") + "\nAdgangskode: " + password);
+        EmailProvider.sendEmail(new Email(json.getString("email")), "Velkommen til CrMS!", content);
 
     }
 }
