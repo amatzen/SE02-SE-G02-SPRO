0bbaa12f26f424eb86d5b7323b2eee933d6759b3
Ashvikan Sivabalaa <ashvikan10@gmail.com>
2021-04-15 12:26:02 +0200

    Person and PersonContactDetails

---
+++ src/main/java/dk/sdu/swe/models/Person.java
@@ -0,0 +1,35 @@
+package dk.sdu.swe.models;
+
+
+public class Person {
+    private int id;
+    private String name;
+    private String dateOfBirth;
+    private String image;
+    private PersonContactDetails contactDetails;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public PersonContactDetails getContactDetails() {
+        return contactDetails;
+    }
+
+}
+
+
+
