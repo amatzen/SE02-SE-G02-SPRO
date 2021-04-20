c100d191f6856978df65820e4a3a16f7e8dcbf1c
Daniel Bahrami <daniel@bahrami.dk>
2021-04-20 23:23:29 +0200

    Programme update
    New getters, setters & constructor

--- src/main/java/dk/sdu/swe/models/EPGProgramme.java
+++ src/main/java/dk/sdu/swe/models/EPGProgramme.java
@@ -1,7 +1,63 @@
 package dk.sdu.swe.models;
 
+import java.util.List;
+import java.util.Map;
+
 public class EPGProgramme {
 
+    private String start;
+    private String stop;
+    private List<String> categories;
+    private String epgIdentifier;
+    private String epgTitle;
+    private Map<String, Boolean> options;
 
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getStop() {
+        return stop;
+    }
+
+    public void setStop(String stop) {
+        this.stop = stop;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    public String getEpgIdentifier() {
+        return epgIdentifier;
+    }
+
+    public void setEpgIdentifier(String epgIdentifier) {
+        this.epgIdentifier = epgIdentifier;
+    }
+
+    public String getEpgTitle() {
+        return epgTitle;
+    }
+
+    public void setEpgTitle(String epgTitle) {
+        this.epgTitle = epgTitle;
+    }
+
+    public Map<String, Boolean> getOptions() {
+        return options;
+    }
+
+    public void setOptions(Map<String, Boolean> options) {
+        this.options = options;
+    }
 
 }
--- src/main/java/dk/sdu/swe/models/Programme.java
+++ src/main/java/dk/sdu/swe/models/Programme.java
@@ -1,7 +1,80 @@
 package dk.sdu.swe.models;
 
+import java.util.List;
+import java.util.Map;
+
 public class Programme {
 
+    private int id;
+    private String title;
+    private String channel;
+    private int prodYear;
+    private String category;
+    private List<EPGProgramme> epgDates;
+    private Map<String, Person> people;
 
+    public Programme(int id, String title, String channel, int prodYear, String category) {
+        this.id = id;
+        this.title = title;
+        this.channel = channel;
+        this.prodYear = prodYear;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public int getProdYear() {
+        return prodYear;
+    }
+
+    public void setProdYear(int prodYear) {
+        this.prodYear = prodYear;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public List<EPGProgramme> getEpgDates() {
+        return epgDates;
+    }
+
+    public void setEpgDates(List<EPGProgramme> epgDates) {
+        this.epgDates = epgDates;
+    }
+
+    public Map<String, Person> getPeople() {
+        return people;
+    }
+
+    public void setPeople(Map<String, Person> people) {
+        this.people = people;
+    }
 
 }
