13f726ce5a3379e46b8ed82c70ab90bfe626157a
Daniel Bahrami <daniel@bahrami.dk>
2021-04-20 23:07:37 +0200

    Deletion

--- src/main/java/dk/sdu/swe/views/ProgrammesViewController.java
+++ src/main/java/dk/sdu/swe/views/ProgrammesViewController.java
@@ -1,21 +1,13 @@
 package dk.sdu.swe.views;
 
-import dk.sdu.swe.models.Programme;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Objects;
 
 public class ProgrammesViewController extends BorderPane {
 
     public ProgrammesViewController() {
-
-        final ArrayList<String> channels = new ArrayList<String>();
-        final ArrayList<String> category = new ArrayList<String>();
-
-        Programme Test = new Programme(1,"Gutterne På Kutterne","TV2",2021,"Komedie");
-        Programme Test2 = new Programme(2,"Gutterne På Kutterne 2", "TV2",2021,"Komedie");
 
         FXMLLoader fxmlLoader = new FXMLLoader(
             Objects.requireNonNull(
