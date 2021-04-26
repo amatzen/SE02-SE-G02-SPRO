e941ec9c879e92dfdc974d86e1183a8916af4d2e
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2021-04-26 12:48:15 +0200

    Fixed user test

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -11,28 +11,28 @@ public class UserTest {
     @Test
     void testUserCreation() {
         assertDoesNotThrow(() -> {
-            new User(1, "almat20", "alexander@alexander.dk", "Alexander Matzen", );
+            new User(1, "almat20", "alexander@alexander.dk", "Alexander Matzen", 1);
         });
     }
 
     @Test
     void testUserCreationWithIncorrectName() {
         assertThrows(InvalidNameException.class, () -> {
-            new User(2, "almat20", "alexander@alexander.dk", "Alexander", );
+            new User(2, "almat20", "alexander@alexander.dk", "Alexander", 1);
         });
     }
 
     @Test
     void testUserCreationWithIncorrectEmail() {
         assertThrows(UserCreationException.class, () -> {
-            new User(3, "almat20", "alexanderalexander.dk", "Alexander Matzen", );
+            new User(3, "almat20", "alexanderalexander.dk", "Alexander Matzen", 1);
         });
     }
 
     @Test
     void testUserCreationWithTooShortUsername() {
         assertThrows(UserCreationException.class, () -> {
-           new User(4, "am", "alexander@alexander.dk", "Alexander Matzen", );
+           new User(4, "am", "alexander@alexander.dk", "Alexander Matzen", 1);
         });
     }
 }
