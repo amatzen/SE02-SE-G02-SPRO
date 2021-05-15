73f2ab671bc633f8fd1ec998cbb8ae9fd48a08cb
Alexander Matzen <alexander@amatzen.dk>
2021-05-15 19:06:07 +0200

    Changed init company to TV 2

--- src/main/java/dk/sdu/swe/data/seeders/v0_CreateCompanies.java
+++ src/main/java/dk/sdu/swe/data/seeders/v0_CreateCompanies.java
@@ -9,8 +9,8 @@ public class v0_CreateCompanies {
     public static void run() throws Exception {
         IDAO<Company> dao = CompanyDAOImpl.getInstance();
 
-        dao.save(new Company("Company1", null,
-            new CompanyDetails("Denmark", "Whateva", "YeaYea"), "LogoGoBrr"));
+        dao.save(new Company("TV2/DANMARK A/S", null,
+            new CompanyDetails("Denmark", "529900PMO5IBS8IO2N04", "10413494"), "https://pbs.twimg.com/profile_images/469398737484144640/o1P1XYg8.png"));
 
     }
 }
