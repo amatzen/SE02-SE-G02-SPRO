2617c0e0ed62245513e88c0965520554e0f210a4
Alexander Matzen <alexander@amatzen.dk>
2021-05-15 22:39:04 +0200

    Added things to Review

--- src/main/java/dk/sdu/swe/domain/models/Review.java
+++ src/main/java/dk/sdu/swe/domain/models/Review.java
@@ -1,8 +1,12 @@
 package dk.sdu.swe.domain.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import dk.sdu.swe.data.converters.JSONConverter;
+import dk.sdu.swe.data.converters.ProgrammeJSONConverter;
+import dk.sdu.swe.domain.controllers.AuthController;
+import org.joda.time.DateTime;
+import org.json.JSONObject;
+
+import javax.persistence.*;
 
 @Entity
 public class Review {
@@ -10,6 +14,62 @@ public class Review {
     @GeneratedValue
     private Integer id;
 
+    @ManyToOne
+    private Programme programme;
 
+    @ManyToOne
+    private User user;
 
+    private DateTime submission_time;
+
+    @Convert(converter = JSONConverter.class)
+    private JSONObject newProgramme;
+
+    private ReviewState state;
+
+    public Review() {
+    }
+
+    public Review(Programme programme, JSONObject newProgramme) {
+        this.programme = programme;
+        this.user = AuthController.getInstance().getUser();
+        this.submission_time = DateTime.now();
+        this.newProgramme = newProgramme;
+
+        this.state = ReviewState.AWAITING;
+    }
+
+    public void setState(ReviewState state) {
+        this.state = state;
+    }
+
+    public ReviewState getState() {
+        return state;
+    }
+
+    public Programme getProgramme() {
+        return programme;
+    }
+
+    public DateTime getSubmission_time() {
+        return submission_time;
+    }
+
+    public JSONObject getNewProgramme() {
+        return newProgramme;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
+
+enum ReviewState {
+    AWAITING,
+    ACCEPTED,
+    DENIED
 }
