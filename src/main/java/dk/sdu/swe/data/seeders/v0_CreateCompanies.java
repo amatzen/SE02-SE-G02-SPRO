0498c12abad71d6721a8ce9127342fa5fa960f54
Alexander Matzen <alexander@amatzen.dk>
2021-05-15 19:43:29 +0200

    Added new companies

--- src/main/java/dk/sdu/swe/data/seeders/v0_CreateCompanies.java
+++ src/main/java/dk/sdu/swe/data/seeders/v0_CreateCompanies.java
@@ -12,5 +12,56 @@ public class v0_CreateCompanies {
         dao.save(new Company("TV2/DANMARK A/S", null,
             new CompanyDetails("Denmark", "529900PMO5IBS8IO2N04", "10413494"), "https://pbs.twimg.com/profile_images/469398737484144640/o1P1XYg8.png"));
 
+        dao.save(
+            new Company(
+                "Banijay Group",
+                null,
+                new CompanyDetails(
+                    "France",
+                    "969500TVFVKI682L1144",
+                    "499797041"
+                ),
+            "https://screenshots.amatzen.dk/gicyaqEXonuKwOV02M.png"
+            )
+        );
+
+        dao.save(
+            new Company(
+                "Nordisk Film TV A/S",
+                CompanyDAOImpl.getInstance().getById(2L).orElse(null),
+                new CompanyDetails(
+                    "Denmark",
+                    "",
+                    ""
+                ),
+                "https://screenshots.amatzen.dk/gicyaqEXonuKwOV02M.png"
+            )
+        );
+
+        dao.save(
+            new Company(
+                "Pineapple Entertainment ApS",
+                CompanyDAOImpl.getInstance().getById(2L).orElse(null),
+                new CompanyDetails(
+                    "Denmark",
+                    "",
+                    ""
+                ),
+                "https://screenshots.amatzen.dk/gicyaqEXonuKwOV02M.png"
+            )
+        );
+
+        dao.save(
+            new Company(
+                "Blu A/S",
+                null,
+                new CompanyDetails(
+                    "Denmark",
+                    "",
+                    "28296819"
+                ),
+                "https://pbs.twimg.com/profile_images/3211552481/d697214c25469d5b4a2c751c25222cf0_400x400.jpeg"
+            )
+        );
     }
 }
