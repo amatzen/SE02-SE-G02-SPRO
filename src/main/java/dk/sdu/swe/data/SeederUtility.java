cb2e5b1b8b8fef1834328e8516d087043773612e
Alexander Matzen <alexander@amatzen.dk>
2021-05-17 00:25:43 +0200

    Refactor

--- src/main/java/dk/sdu/swe/data/SeederUtility.java
+++ src/main/java/dk/sdu/swe/data/SeederUtility.java
@@ -9,11 +9,18 @@ public class SeederUtility {
             v0_CreateCompanies.run();
             v1_CreateUsers.run();
             v2_CreateChannels.run();
-            v3_CreateProgrammesForThisWeek.run(DateTime.now().toString("yyyy-MM-dd"));
-            for (int i = 1; i < 8; i++) {
-                v3_CreateProgrammesForThisWeek.run(DateTime.now().plusDays(i).toString("yyyy-MM-dd"));
-            }
-            v4_CreateCreditRoles.run();
+
+            new Thread(() -> {
+                try {
+                    v3_CreateProgrammesForThisWeek.run(DateTime.now().toString("yyyy-MM-dd"));
+                    for (int i = 1; i < 8; i++) {
+                        v3_CreateProgrammesForThisWeek.run(DateTime.now().plusDays(i).toString("yyyy-MM-dd"));
+                    }
+                } catch (Exception e) {
+                }
+            }).start();
+
+            v2_1_CreateCreditRoles.run();
         }
         catch (Exception e) {
             e.printStackTrace();
