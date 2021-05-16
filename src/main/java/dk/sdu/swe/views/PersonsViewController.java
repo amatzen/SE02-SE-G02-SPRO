c274bbb12bb743ad10c04ebb5b4708f8a84ff043
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2021-05-17 00:00:32 +0200

    Fixed null pointer exception for unassigned image urls on new people

--- src/main/java/dk/sdu/swe/views/PersonsViewController.java
+++ src/main/java/dk/sdu/swe/views/PersonsViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.layout.VBox;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class PersonsViewController extends VBox {
 
@@ -57,7 +58,10 @@ public class PersonsViewController extends VBox {
     @FXML
     private void addPersonModal(ActionEvent event) {
         Dialog<Person> addPerson = new PersonModal(getScene().getWindow());
-        addPerson.show();
+        Optional<Person> person = addPerson.showAndWait();
+        person.ifPresent(personObj -> {
+            peopleListView.getItems().add(new PersonListItem(personObj, peopleListView));
+        });
     }
 
     @FXML
