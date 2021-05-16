ed2e63af94245e3e605096cfa2f145b073d7000d
Alexander Matzen <alexander@amatzen.dk>
2021-05-16 23:56:37 +0200

    esben made it a set :(

--- src/main/java/dk/sdu/swe/data/seeders/v3_CreateProgrammesForThisWeek.java
+++ src/main/java/dk/sdu/swe/data/seeders/v3_CreateProgrammesForThisWeek.java
@@ -117,7 +117,7 @@ public class v3_CreateProgrammesForThisWeek {
                         epgObj.getString("title"),
                         channelDAO.getByEpgId(channelObj.getLong("id")).orElse(null),
                         0,
-                        categories.stream().filter(distinctByKey(Category::getCategoryTitle)).collect(Collectors.toList()),
+                        new HashSet(categories.stream().filter(distinctByKey(Category::getCategoryTitle)).collect(Collectors.toList())),
                         null
                     );
 
