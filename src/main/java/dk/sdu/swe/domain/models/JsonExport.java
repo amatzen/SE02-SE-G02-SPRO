5c70a7a397fb4eb073e69ea748982a87dec8e557
Daniel Bahrami <daniel@bahrami.dk>
2021-05-17 11:18:36 +0200

    Added JsonExport class

---
+++ src/main/java/dk/sdu/swe/domain/models/JsonExport.java
@@ -0,0 +1,14 @@
+package dk.sdu.swe.domain.models;
+
+public class JsonExport {
+
+    public static void JsonExport() {
+
+        try {
+            System.out.println("JsonExport kode her");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
