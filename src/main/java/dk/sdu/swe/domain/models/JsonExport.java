53668adbce281ff4f1b48d21068d018d2c6b6d1b
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2021-05-18 18:32:42 +0200

    Data export view fix

--- src/main/java/dk/sdu/swe/domain/models/JsonExport.java
+++ src/main/java/dk/sdu/swe/domain/models/JsonExport.java
@@ -1,15 +1,27 @@
 package dk.sdu.swe.domain.models;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dk.sdu.swe.domain.controllers.CreditController;
+
 import java.io.File;
 import java.io.FileWriter;
+import java.util.List;
 
 public class JsonExport {
 
     public static void JsonExportCredits(File file) {
 
+        Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
+
+        List<Credit> credits = CreditController.getInstance().getAll();
+
         try {
             FileWriter jsonWriter = new FileWriter(file);
-            // Kode til JSON eksportering her
+            jsonWriter.write(gson.toJson(credits));
+            jsonWriter.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
