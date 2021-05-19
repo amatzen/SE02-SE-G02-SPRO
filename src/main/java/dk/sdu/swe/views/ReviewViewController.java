f03cfd074d460ebf6b09ce334216b626921fbff1
Alexander Matzen <alexander@amatzen.dk>
2021-05-19 22:02:00 +0200

    Only show awaiting reviews

--- src/main/java/dk/sdu/swe/views/ReviewViewController.java
+++ src/main/java/dk/sdu/swe/views/ReviewViewController.java
@@ -3,6 +3,9 @@ package dk.sdu.swe.views;
 import com.jfoenix.controls.JFXListView;
 import dk.sdu.swe.domain.controllers.ReviewController;
 import dk.sdu.swe.domain.models.Review;
+import dk.sdu.swe.domain.models.ReviewState;
+import dk.sdu.swe.helpers.Observer;
+import dk.sdu.swe.helpers.PubSub;
 import dk.sdu.swe.views.partials.ReviewListItem;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,12 +15,16 @@ import javafx.scene.layout.VBox;
 import java.io.IOException;
 import java.util.Objects;
 
-public class ReviewViewController extends AnchorPane {
+public class ReviewViewController extends AnchorPane implements Observer {
+
+
 
     @FXML
     private JFXListView<ReviewListItem> reviews;
 
     public ReviewViewController() {
+        PubSub.subscribe("trigger_refresh:admin:reviews", this);
+
         FXMLLoader fxmlLoader = new FXMLLoader(
             Objects.requireNonNull(
                 getClass().getClassLoader().getResource("dk/sdu/swe/views/admin/components/ReviewView.fxml")));
@@ -37,14 +44,17 @@ public class ReviewViewController extends AnchorPane {
 
     @FXML
     private void initialize() {
-        System.out.println("TEST");
-        ReviewController.getInstance().getAll().forEach(x -> {
-            System.out.println(x.toString());
+        ReviewController.getInstance().getAll().stream().filter(x -> x.getState() == ReviewState.AWAITING).forEach(x -> {
             try {
                 createListItem(x);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
+    }
+
+    @Override
+    public void onNotify(String topic, Object payload) {
+        review
     }
 }
