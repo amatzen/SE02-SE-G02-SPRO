db0a634da61c745083d4635489956ccca1ad6cbb
Alexander Matzen <alexander@amatzen.dk>
2021-05-20 08:47:22 +0200

    Added Approve and Decline func to ReviewView

--- src/main/java/dk/sdu/swe/views/partials/ReviewListItem.java
+++ src/main/java/dk/sdu/swe/views/partials/ReviewListItem.java
@@ -2,8 +2,13 @@ package dk.sdu.swe.views.partials;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
+import dk.sdu.swe.data.dao.ReviewDAOImpl;
+import dk.sdu.swe.domain.controllers.ReviewController;
 import dk.sdu.swe.domain.models.Person;
 import dk.sdu.swe.domain.models.Review;
+import dk.sdu.swe.domain.models.ReviewState;
+import dk.sdu.swe.helpers.PubSub;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -50,5 +55,20 @@ public class ReviewListItem extends VBox {
         lbl_ProgramTitle.setText(review.getProgramme().getTitle());
         lbl_RequesterName.setText(review.getUser().getName().toString());
         lbl_RequesterCompany.setText(review.getUser().getCompany().getName());
+
+        btn_ApproveReview.setOnAction(this::approve);
+        btn_DeclineReview.setOnAction(this::decline);
+    }
+
+    private void approve(ActionEvent event) {
+        review.setState(ReviewState.ACCEPTED);
+        ReviewController.getInstance().update(review);
+        PubSub.publish("trigger_refresh:admin:reviews", true);
+    }
+
+    private void decline(ActionEvent event) {
+        review.setState(ReviewState.DENIED);
+        ReviewController.getInstance().update(review);
+        PubSub.publish("trigger_refresh:admin:reviews", true);
     }
 }
