b14bb5e3ca76382cd0df324705bcc6b58d825e30
Alexander Matzen <alexander@amatzen.dk>
2021-05-25 18:34:30 +0200

    Consumer System

---
+++ consumer_system/ext/webpack.production.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        app: path.join(__dirname, '/src/app.js'),
+        cs_tvtid: path.join(__dirname, '/src/injections/tvtid/main.js'),
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    },
+                    'sass-loader',
+                    'resolve-url-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'resolve-url-loader'
+                ]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'static' }
+            ]
+        })
+    ]
+}
